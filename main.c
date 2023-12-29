009eaac8065a3ecc2dd22aa656275faa6b996690
sellami mohamed ayoub <152229661+sellamiayoub@users.noreply.github.com>
2023-12-29 19:23:11 +0100

    button
    Button

--- main.c
+++ main.c
@@ -1,11 +1,38 @@
-#include"raylib.h"
-void main(){
-    InitWindow(800,800,"Project");
-    while (!WindowShouldClose)
-    {
+#include "raylib.h"
+
+int main() {
+    // Initialiser la fenêtre
+    const int screenWidth = 800;
+    const int screenHeight = 800;
+
+    InitWindow(screenWidth, screenHeight, "Projet TP ");
+
+    // Initialiser le bouton
+    Rectangle buttonBounds = { screenWidth / 2 -75 , screenHeight/4-150, 150, 70 };
+    bool buttonPressed = false;
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        // Mettre a jour la logique du bouton
+        if (CheckCollisionPointRec(GetMousePosition(), buttonBounds) && IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+            buttonPressed = true;
+        } else {
+            buttonPressed = false;
+        }
+
+        // Début du rendu
         BeginDrawing();
 
+        ClearBackground(RAYWHITE);
+
+        // Dessiner le bouton
+        DrawRectangleRec(buttonBounds, buttonPressed ? DARKGRAY : BLACK);
+        DrawText("trie ", (int)(buttonBounds.x + buttonBounds.width / 2 - MeasureText("trie", 20) / 2),
+                 (int)(buttonBounds.y + buttonBounds.height / 2 - 10), 20, buttonPressed ? DARKGRAY : GRAY);
         EndDrawing();
     }
     CloseWindow();
+
+    return 0;
 }
