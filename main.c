7873fa009a4bf1d4c3e3bd39ef03774aef3c5957
its0amine <mohamedamine.heddouche@etu.usthb.dz>
2023-12-29 16:04:06 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include"raylib.h"
+void main(){
+    InitWindow(800,800,"Project");
+    while (!WindowShouldClose)
+    {
+        BeginDrawing();
+
+        EndDrawing();
+    }
+    CloseWindow();
+}
